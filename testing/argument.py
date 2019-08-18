66660fe054b6c0a3a2ea117a1d8bdd2ef7aeee23
SamHDev-Ubuntu <sam02h.huddart@gmail.com>
2019-08-18 02:03:34 +0100

    Auto Push

--- testing/argument.py
+++ testing/argument.py
@@ -1,5 +1,7 @@
 input_text = "how do you spell London Bridge"
 
+input_text = "set value name to 10"
+
 import re
 
 intents = []
@@ -15,9 +17,14 @@ class Intent:
         self.phrases = []
         self.arguments = []
 
+
 # regex, Allow Multiple
 def word():
     return r"([a-zA-Z]*)", True
+
+
+def number():
+    return r"([0-9]*)", False
 
 
 intents.append(Intent())
@@ -30,6 +37,12 @@ intents[1].arguments.append(["word", word()])
 intents[1].phrases.append("what does the word {word} mean")
 intents[1].phrases.append("define {word}")
 
+intents.append(Intent())
+intents[2].arguments.append(["word", word()])
+intents[2].arguments.append(["number", number()])
+intents[2].phrases.append("set value {word} to {number}")
+intents[2].phrases.append("change value {word} to {number}")
+
 query = input_text
 query_words = query.lower().split(" ")
 similar_threshold = 0.9
@@ -37,31 +50,42 @@ similar_threshold = 0.9
 for intent in intents:
     for phrase in intent.phrases:
         score = 0
-        score_max = len(query_words)
         phrase_query_words = query_words.copy()
         phrase_arguments = {}
-        for argname in re.compile(r"{(.*)}").findall(phrase):
-            phrase_arguments[argname[0]] = []
+        for argname in re.compile(r'\{(.+?)\}').findall(phrase):
+            phrase_arguments[argname] = []
+        print(phrase_arguments)
         for phrase_word in phrase.lower().split(" "):
-            phrase_args = re.compile(r"{(.*)}").findall(phrase_word)
+            phrase_args = re.compile(r'\{(.+?)\}').findall(phrase_word)
             phrase_arg = (len(phrase_args) != 0)
             if phrase_arg:
-                phrase_argname = phrase_args[0][0]
+                phrase_argname = phrase_args[0]
             else:
                 phrase_argname = None
             for query_word in phrase_query_words:
-                #print(query_word,phrase_query_words)
+                # print(query_word,phrase_query_words)
                 similar_result = similar(query_word, phrase_word)
                 if similar_result >= similar_threshold:
                     score = score + 1
-                    for query2 in phrase_query_words:
+                    # print(query_word,phrase_word, phrase_query_words, score)
+                    for i in range(0, phrase_query_words.index(query_word) + 1):
                         phrase_query_words.pop(0)
-                        if query2 == query_word:
-                            break
                     break
-                else:
-                    if (phrase_arg):
+                elif phrase_arg:
+                    found_argument = None
+                    for finder_arg in intent.arguments:
+                        if finder_arg[0] == phrase_argname:
+                            found_argument = finder_arg
+                    if len(re.compile(found_argument[1][0]).findall(query_word)) != 0:
                         phrase_arguments[phrase_argname].append(query_word)
-                        score = score + 1
-                        #phrase_query_words.remove(query_word)
-        print(phrase, score / score_max, score, score_max,phrase_arguments)
+                        score = score + 0.75
+                        print(query_word, phrase_word, phrase_query_words, score)
+                        if found_argument[1][1] == False:
+                            phrase_query_words.remove(query_word)
+                    else:
+                        score = score + 0.25
+                else:
+                    score = score - 0.55
+
+        print(phrase, score, phrase_arguments)
+        print("-" * 50)
