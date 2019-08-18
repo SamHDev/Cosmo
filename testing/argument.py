a57d1c82403e43cde309c24195e83e5fbd84217c
SamHDev-Ubuntu <sam02h.huddart@gmail.com>
2019-08-18 01:02:06 +0100

    Auto Push

--- testing/argument.py
+++ testing/argument.py
@@ -1,4 +1,4 @@
-input_text = "how do you spell London"
+input_text = "how do you spell London Bridge"
 
 import re
 
@@ -15,15 +15,20 @@ class Intent:
         self.phrases = []
         self.arguments = []
 
-
+# regex, Allow Multiple
 def word():
-    return r"([a-zA-Z]*)"
+    return r"([a-zA-Z]*)", True
 
 
 intents.append(Intent())
 intents[0].arguments.append(["word", word()])
 intents[0].phrases.append("how do you spell {word}")
 intents[0].phrases.append("spell {word}")
+
+intents.append(Intent())
+intents[1].arguments.append(["word", word()])
+intents[1].phrases.append("what does the word {word} mean")
+intents[1].phrases.append("define {word}")
 
 query = input_text
 query_words = query.lower().split(" ")
@@ -33,9 +38,10 @@ for intent in intents:
     for phrase in intent.phrases:
         score = 0
         score_max = len(query_words)
+        phrase_query_words = query_words.copy()
         phrase_arguments = {}
-        for argname in re.compile(r"{(.*)}").findall(phrase)
-            phrase_arguments[argname] = []
+        for argname in re.compile(r"{(.*)}").findall(phrase):
+            phrase_arguments[argname[0]] = []
         for phrase_word in phrase.lower().split(" "):
             phrase_args = re.compile(r"{(.*)}").findall(phrase_word)
             phrase_arg = (len(phrase_args) != 0)
@@ -43,13 +49,19 @@ for intent in intents:
                 phrase_argname = phrase_args[0][0]
             else:
                 phrase_argname = None
-            for query_word in query_words:
+            for query_word in phrase_query_words:
+                #print(query_word,phrase_query_words)
                 similar_result = similar(query_word, phrase_word)
                 if similar_result >= similar_threshold:
-                    print("MATCHING WORD", query_word)
                     score = score + 1
-                    query_words.remove(query_word)
+                    for query2 in phrase_query_words:
+                        phrase_query_words.pop(0)
+                        if query2 == query_word:
+                            break
                     break
                 else:
-                    phrase_arg
-        print(phrase, score / score_max, score, score_max)
+                    if (phrase_arg):
+                        phrase_arguments[phrase_argname].append(query_word)
+                        score = score + 1
+                        #phrase_query_words.remove(query_word)
+        print(phrase, score / score_max, score, score_max,phrase_arguments)
