ae2bcea8edb39fc9e4aef5f9834897cf51408911
SamHDev-Ubuntu <sam02h.huddart@gmail.com>
2019-08-23 05:05:51 +0100

    Auto Push

--- testing/argument2.py
+++ testing/argument2.py
@@ -13,6 +13,12 @@ class Intent:
         self.phrases = []
         self.arguments = []
 
+    def find_argument(self, name):
+        for arg in self.arguments:
+            if name == arg.name:
+                return arg
+        return None
+
 
 class IntentArgument:
     def __init__(self, name, atype):
@@ -21,12 +27,26 @@ class IntentArgument:
 
 
 class IntentArgumentType:
-    def __init__(self, name, regex):
+    def __init__(self, name):
         self.name = name
-        self.regex = regex
 
-    def get_regex(self):
-        return re.compile(self.regex)
+    def check(self, query):
+        return True
+
+
+def IntentArgumentTypeFromRegex(name, regex):
+    class SubIntentArgument(IntentArgumentType):
+        def __init__(self):
+            IntentArgumentType.__init__(self, name)
+            self.regex = regex
+
+        def get_regex(self):
+            return re.compile(self.regex)
+
+        def check(self, query):
+            return len(self.get_regex().findall(query)) != 0
+
+    return SubIntentArgument()
 
 
 # Sim Ratio Function
@@ -36,9 +56,11 @@ def similar(a, b): return difflib.SequenceMatcher(None, a, b).ratio()
 def similar_check(a, b, thresh):
     return similar(a, b) >= thresh
 
+
 def similar_check_wres(a, b, thresh):
     check = similar(a, b)
     return check >= thresh, check
+
 
 def similar_check_list(a, blist, thresh):
     for b in blist:
@@ -95,51 +117,165 @@ def find_word_in_group(term, groups, thresh):
         groups_count = groups_count + 1
     return [False, [term, None], None]
 
-def find_middle(location1,location2,grouping):
-    pass
 
-def find_intent(intents, query, s_threshold=0.8):
+def find_middle(loc1, loc2, groups):
+    middle = []
+    loc = [0, 0]
+    for group in groups:
+        loc[1] = 0
+        for group_word in group[0]:
+            if loc1[0] == loc2[0]:
+                if loc1[1] < loc[1] < loc2[1]:
+                    middle.append(loc.copy())
+            else:
+                if loc1[0] == loc[0]:
+                    if loc1[1] < loc[1]:
+                        middle.append(loc.copy())
+                elif loc2[0] == loc[0]:
+                    if loc2[1] > loc[1]:
+                        middle.append(loc.copy())
+                elif loc1[0] < loc[0] < loc2[0]:
+                    middle.append(loc.copy())
+
+            loc[1] += 1
+        loc[0] += 1
+    return middle
+
+
+def find_last(groups):
+    grp = len(groups) - 1
+    return [grp, len(groups[grp][0]) - 1]
+
+
+def find_last_own(groups):
+    grp = len(groups) - 1
+    return [grp, 0]
+
+
+def find_search(intent, phrase, query, thresh):
     query_words = query.lower().split(" ")
-    for intent in intents:  # Loop Through Intents
-        for phrase in intent.phrases:  # Loop Through Phrases:
 
-            # Find Arguments
-            arguments = re.compile("\{([a-zA-Z0-9]+?)\}").findall(phrase)
-            found_arguments = {}
-            for intent_phrase in intent.arguments:
-                found_arguments[intent_phrase.name] = []
+    # Find Arguments
+    arguments = re.compile("\{([a-zA-Z0-9]+?)\}").findall(phrase)
+    found_arguments = {}
+    for intent_phrase in intent.arguments:
+        found_arguments[intent_phrase.name] = []
 
-            # Create Grouping Temp Variables
-            phrase_words = phrase.lower().split(" ")
+    # Create Grouping Temp Variables
+    phrase_words = phrase.lower().split(" ")
 
-            #
-            grouping_a = group_matchs(query_words, phrase_words, s_threshold)
-            grouping_b = group_matchs(phrase_words, query_words, s_threshold)
+    # Find Argument and Phrase Groups
+    grouping_a = group_matchs(query_words, phrase_words, thresh)
+    grouping_b = group_matchs(phrase_words, query_words, thresh)
 
-            grouping_matrix = None
-            for group_b in grouping_b:
-                if group_b[1] == True:
-                    for word in group_b[0]:
-                        print(find_word_in_group(word, grouping_a, s_threshold))
+    # Arguments for Finding Arguments and Resolving Middle Group
+    last_pos = [0, 0]
+    argument_name = None
+    arguments_find = {}
+    score = 0  # Score Var
+
+    # Loop Through Grouping
+    for group_b in grouping_b:  # All Groups
+        if group_b[1]:  # If Group Is Matched Group
+            for word in group_b[0]:  # Sub Groups
+                match_place = find_word_in_group(word, grouping_a, thresh)  # Find Location of word (Buggy)
+                found = find_middle(last_pos, match_place[2], grouping_a)  # Find Middle Arguments
+                # print(found)
+                if len(found) != 0:  # If sub groups in middle
+                    if argument_name is not None:
+                        if argument_name in arguments_find.keys():
+                            arguments_find[argument_name] += found
+                            score += 0.35  # Add Second Weight
+                        else:
+                            arguments_find[argument_name] = found
+                            score += 0.5  # Add Weight
                 else:
-                    if is_arg(group_b[0][0]):
-                        print("Argument: " + group_b[0][0])
-                    else:
-                        print("NO FOUND")
+                    argument_name = None  # Reset Argument Name
+                    score += 1.5  # Base Weight
+
+                last_pos = match_place[2]  # Set last pos
+        else:  # If Group Is Non Matched Group
+            if is_arg(group_b[0][0]):  # If The Sub Group Word is an argument
+                argument_name = get_args(group_b[0][0])[0]  # Get Argument Name with handy dandy argument
+                score += 0.5  # Add Argument Filler Weight
+            else:
+                pass  # Not Found.
+
+    # Last Loop (To catch arguments at end of phrase)
+    last_loc = find_last(grouping_a)
+    last_loc[1] += 1
+    found = find_middle(last_pos, last_loc, grouping_a)  # Find Location of word (Still Buggy)
+    # print(found)
+    if len(found) != 0:  # If sub groups in middle
+        if argument_name is not None:
+            if argument_name in arguments_find.keys():
+                arguments_find[argument_name] += found
+            else:
+                arguments_find[argument_name] = found
+    else:
+        argument_name = None
+
+    # Resolve Argument Words
+    argument_data = {}
+    for arg_name in arguments_find:  # Loop Through Arguments
+        arg_data = []
+        for arg_loc in arguments_find[arg_name]:  # Loop Through Sub groups
+            arg_data.append(grouping_a[arg_loc[0]][0][arg_loc[1]])  # Add with messy line of code
+
+        if len(arg_data) == 0:
+            score -= 1
+
+        arg_data = " ".join(arg_data)  # Join words (with spaces)
+        arg = intent.find_argument(arg_name)
+
+        # Check Arg Types
+        if arg.atype.check(arg_data):
+            score += 0.25  # Add Argument Filler Weight
+        else:
+            score -= 1  # Add Argument Filler Weight
+        argument_data[arg_name] = arg_data
+
+    # Finish Up
+    score = round(score, 4)
+    return score, argument_data
+
+
+def find_intent(intents, query, s_threshold=0.8, r_threshold=4):
+    results = []  # Results Var
+
+    for intent in intents:  # Loop Through Intents
+        for phrase in intent.phrases:  # Loop Through Phrases
+
+            # Search
+            score, args = find_search(intent, phrase, query, s_threshold)
+            results.append([score, intent, phrase, args])
+
+    # Search Through All Results
+    best = [0, None, None, {}]
+    for res in results:
+        if res[0] > best[0]:
+            best = res
+
+    if best[0] < r_threshold:
+        return False, None, best[0], None
+
+    # Return (Intent,Score,Args)
+    # print(best)
+    return True, best[1], best[0], best[3]
 
 
 # Demo Stuff
 
-string = IntentArgumentType("String", r"([a-zA-Z0-9]*)")
+string = IntentArgumentTypeFromRegex("String", r"([a-zA-Z0-9]*)")
 
 intent_list = []
 
 intent = Intent()
 intent.phrases.append("whats the time in {city} along {city2}")
-# intent.phrases.append("what time is it in {city} along {city2}")
+intent.phrases.append("what time is it in {city} along {city2}")
 intent.arguments.append(IntentArgument("city", string))
 intent.arguments.append(IntentArgument("city2", string))
 
 intent_list.append(intent)
 
-find_intent(intent_list, "whats the time cosmo in London Bridge along epic gamer")
+print(find_intent(intent_list, "whats the time in London Bridge along epic gamer"))
