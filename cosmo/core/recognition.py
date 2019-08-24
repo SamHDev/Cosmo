97baba800d1562142594e3b9b3304459279e7a14
SamHDev-Ubuntu <sam02h.huddart@gmail.com>
2019-08-24 13:09:11 +0100

    Gleb is a bad minecrafter

--- cosmo/core/recognition.py
+++ cosmo/core/recognition.py
@@ -1,8 +1,3 @@
-# Copyright (C) SamHDev, Inc - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-# Written by Sam Huddart <sam02h.huddart@gmail.com>, August 2019
-# Licensed to CosmoHome on a Temporary Basis. This may be revoked at any time.
 
 import speech_recognition as sr
 import pocketsphinx
