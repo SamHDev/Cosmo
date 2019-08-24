a61b0e7f627cab625c8e2cf23f99bcd932fa0d9d
SamHDev-Ubuntu <sam02h.huddart@gmail.com>
2019-08-24 13:07:29 +0100

    New Logger Config and Interchangeable Styles to please our overlord gleb

--- cosmo/core/logger.py
+++ cosmo/core/logger.py
@@ -1,17 +1,42 @@
-# Copyright (C) SamHDev, Inc - All Rights Reserved
-# Unauthorized copying of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-# Written by Sam Huddart <sam02h.huddart@gmail.com>, August 2019
-# Licensed to CosmoHome on a Temporary Basis. This may be revoked at any time.
-
 import sys
 import datetime
 
+# Logger Config Handler
+logger_default_appname = "Cosmo"
+logger_config = {"format": 0, "debug": True, "color": True}
+import json
 
-# Better Logger than Glebs Logger. Move to New File and formatted nicely. Take that gleb.
+try:
+    logger_config = json.load(open("data/config/logger.json"))
+except:
+    pass  # Something Went Wrong, Oh well, default it is then.
 
 logger_colors = dict(reset="\u001b[0m", white="\u001b[37m", black="\u001b[30m", red="\u001b[31m", green="\u001b[32m",
                      yellow="\u001b[33m", blue="\u001b[34m", purple="\u001b[35m", cyan="\u001b[36m")
+
+logger_styles = [
+    {
+        "root": "{app}> {color_pre} {time} [{type}] {msg}{color_suf}",
+        "info": "INFO",
+        "ok": "-OK-",
+        "warn": "WARN",
+        "error": "ERRR",
+        "fatal": "FATAL",
+        "debug": "Debug",
+        "separator": "-"*70
+    },
+    {
+        "root": "{color_pre} {time}/{app} {type} {msg}{color_suf}",
+        "info": "   ",
+        "ok": "-- ",
+        "warn": "!  ",
+        "error": "!! ",
+        "fatal": "!!!",
+        "debug": ">  ",
+        "separator": "-"*70
+    }
+]
+
 
 def _log(text):
     sys.stdout.write(text + "\n")
@@ -19,46 +44,65 @@ def _log(text):
 
 
 class Logger:
-    def __init__(self, debug=False, app_name=None):
+    def __init__(self, debug=None, app_name=None, config=logger_config):
         self.print_debug = debug
         self.app_name = app_name
+        self.config = config
+
+        if self.print_debug is None:
+            self.print_debug = self.config["debug"]
 
     def _format(self, ltype, msg, app_name=None, color=None):
         if app_name is None:
             if self.app_name is None:
-                app_name = "Cosmo"
+                app_name = logger_default_appname
             else:
                 app_name = self.app_name
+        if ltype in logger_styles[self.config["style"]].keys():
+            ltype = logger_styles[self.config["style"]][ltype]
 
         f_time = datetime.datetime.utcnow().strftime("%H:%M:%S")
         color_pre = ""
         color_suf = ""
+        if not self.config["color"]:
+            color = None
         if color is not None:
             if color in logger_colors.keys():
                 color_pre = logger_colors[color]
             else:
                 color_pre = color
             color_suf = logger_colors["reset"]
-        return "{app}> {color_pre} {time} [{type}] {msg}{color_suf}".format(app=app_name, time=f_time, type=ltype, msg=msg, color_pre=color_pre, color_suf=color_suf)
+        return logger_styles[self.config["style"]]["root"].format(app=app_name, time=f_time, type=ltype,
+                                                                            msg=msg, color_pre=color_pre,
+                                                                            color_suf=color_suf)
 
     def log(self, log_type, log_msg, app_name=None, color=None):
         _log(self._format(log_type, log_msg, app_name=app_name, color=color))
 
+    def __call__(self, msg):
+        return self.debug(msg)
+
     def info(self, msg):
-        self.log("INFO", msg, color="cyan")
+        self.log("info", msg, color="cyan")
 
     def ok(self, msg):
-        self.log("-OK-", msg, color="green")
+        self.log("ok", msg, color="green")
 
     def warn(self, msg):
-        self.log("WARN", msg, color="yellow")
+        self.log("warn", msg, color="yellow")
 
     def error(self, msg):
-        self.log("ERRR", msg, color="red")
+        self.log("error", msg, color="red")
+
+    def fatal(self, msg):
+        self.log("fatal", msg, color="purple")
 
     def debug(self, msg):
         if self.print_debug:
-            self.log("DBUG", msg)
+            self.log("debug", msg)
 
     def sep(self):
-        print("-" * 70)
+        self.separator()
+
+    def separator(self):
+        _log(logger_styles[self.config["style"]]["separator"])
