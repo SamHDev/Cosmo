73dab8c9fbbc9b6ae0e266bdb64540d1c4fba651
Gleb Koval <glebkoval4@gmail.com>
2019-08-26 04:59:48 +0300

    Fixed an issue in demo skill

--- skills/demo/skill.py
+++ skills/demo/skill.py
@@ -3,7 +3,7 @@ import cosmo.api as cosmo
 api = cosmo.API()
 
 class TestSkill(api.Skill):
-    @api.IntentHandler(phrases=("time"))
+    @api.IntentHandler(phrases=("time",))
     def time_intent_callback(self, msg):
         api.logger.debug(f"msg: {msg}")
 
